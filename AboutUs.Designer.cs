dd7d3726f91697b74a17753a43630f63afa6f8b2
thj1234 <thJimenez@mcm.edu.ph>
2024-11-07 15:09:17 +0800

    about us

--- AboutUs.Designer.cs
+++ AboutUs.Designer.cs
@@ -29,13 +29,14 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutUs));
             picVehicleImage = new PictureBox();
             imageList1 = new ImageList(components);
-            pictureBox1 = new PictureBox();
-            pictureBox2 = new PictureBox();
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)picVehicleImage).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
@@ -43,10 +44,10 @@
             // 
             // picVehicleImage
             // 
-            picVehicleImage.BorderStyle = BorderStyle.FixedSingle;
+            picVehicleImage.Image = (Image)resources.GetObject("picVehicleImage.Image");
             picVehicleImage.Location = new Point(45, 52);
             picVehicleImage.Name = "picVehicleImage";
-            picVehicleImage.Size = new Size(88, 80);
+            picVehicleImage.Size = new Size(80, 80);
             picVehicleImage.SizeMode = PictureBoxSizeMode.StretchImage;
             picVehicleImage.TabIndex = 21;
             picVehicleImage.TabStop = false;
@@ -57,63 +58,63 @@
             imageList1.ImageSize = new Size(16, 16);
             imageList1.TransparentColor = Color.Transparent;
             // 
-            // pictureBox1
-            // 
-            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
-            pictureBox1.Location = new Point(45, 177);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(88, 80);
-            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox1.TabIndex = 22;
-            pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            pictureBox2.BorderStyle = BorderStyle.FixedSingle;
-            pictureBox2.Location = new Point(45, 310);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(88, 80);
-            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox2.TabIndex = 23;
-            pictureBox2.TabStop = false;
-            // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(139, 177);
+            label1.Location = new Point(139, 193);
             label1.Name = "label1";
-            label1.Size = new Size(112, 15);
+            label1.Size = new Size(128, 45);
             label1.TabIndex = 24;
-            label1.Text = "Jonathan Damalerio";
+            label1.Text = "Jonathan Damalerio\r\nTester\r\ndamalerio@gmail.com";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(139, 52);
+            label2.Location = new Point(139, 67);
             label2.Name = "label2";
-            label2.Size = new Size(151, 15);
+            label2.Size = new Size(151, 45);
             label2.TabIndex = 25;
-            label2.Text = "Zachary Ian Pelayo Bautista";
+            label2.Text = "Zachary Ian Pelayo Bautista\r\nSQL Developer\r\nbautista@gmail.com";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(139, 310);
+            label3.Location = new Point(139, 325);
             label3.Name = "label3";
-            label3.Size = new Size(146, 15);
+            label3.Size = new Size(148, 45);
             label3.TabIndex = 26;
-            label3.Text = "Thomas Harvey S. Jimenez";
+            label3.Text = "Thomas Harvey S. Jimenez\r\nWindows Forms Developer\r\njimenez@gmail.com";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(45, 177);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(80, 80);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 27;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(45, 310);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(80, 80);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 28;
+            pictureBox2.TabStop = false;
             // 
             // AboutUs
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(351, 450);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
-            Controls.Add(pictureBox2);
-            Controls.Add(pictureBox1);
             Controls.Add(picVehicleImage);
             Name = "AboutUs";
             Text = "AboutUs";
@@ -128,10 +129,10 @@
 
         private PictureBox picVehicleImage;
         private ImageList imageList1;
-        private PictureBox pictureBox1;
-        private PictureBox pictureBox2;
         private Label label1;
         private Label label2;
         private Label label3;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
     }
 }
